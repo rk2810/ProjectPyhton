6f67b2099fd929ed68182ed03fcdfe7168706839
rk2810 <rohan.kumquat@gmail.com>
2017-02-27 15:05:38 +0530

    Getting acquainted to python

---
+++ basics.py
@@ -0,0 +1,18 @@
+#Playing with python and printing the shit
+one = 1
+two = 2
+three = one + two
+
+print(three)
+
+#A play of words :p
+hello = "Hello"
+world = "world"
+helloworld =  hello+" "+world
+print(helloworld)
+
+#multi value print 
+a, b  = 3, 5
+print(a,b)
+
+
---
+++ dataTypes.py
@@ -0,0 +1,15 @@
+#To see all kind of data types 
+myint = 7
+print(myint)
+#float value
+myfloat = 10.111111111111111
+print(myfloat)
+#making float from int
+myfloat = float(10)
+print(myfloat)
+#strings
+mystring = "Hellooooo"
+print(mystring)
+mystring = 'helloo'
+print(mystring)
+
